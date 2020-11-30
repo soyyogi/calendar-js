61a07b9133324fd298238a655b18006f1f024dfe
yogi <raoyogesh78@yahoo.in>
2020-11-30 23:11:33 +0100

    add current month using js #6

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const currentDate = new Date();
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+document.querySelector('.current_month').innerHTML = months[currentDate.getMonth()];
+
