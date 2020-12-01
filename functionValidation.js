4d3a2b98202bfc362570d07b949a2e3571d3bc53
wsierra010 <wsierra010@gmail.com>
2020-12-01 13:44:56 +0100

    Function End Date and Remind #3

--- functionValidation.js
+++ functionValidation.js
@@ -1,32 +1,51 @@
 
 const formulario = document.getElementById("formulario");
-const eventInput = document.getElementById("title_input");
-const dateInput = document.getElementById("date_input");
-const endDate = document.getElementById("end_date");
+// END DATE VARIABLES 
+const endDate = document.getElementById("newDateEnd");
+const checkDate = document.getElementById("checked_endDate");
+// REMIND EVENT VARIABLES
+const reminder = document.getElementById("remindChoose");
+const checkReminder = document.getElementById("check_remind");
 
 
 formulario.addEventListener("submit", (e) => {
     e.preventDefault();
 })
 
-eventInput.addEventListener("keyup", titleValidation);
-var titleValidation = function(){
-    var patternUsername = /^[a-zA-Z0-9][^\s]*$/;
-    if(patternUsername.test(eventInput.value)){
-        
+
+// APPEAR OR DISAPPEAR DATE END CHOOSE
+function on(){
+    endDate.style.display = "block";
+  }
+  
+  function off(){
+    endDate.style.display = "none";
+  }
+  
+  checkDate.addEventListener("change", finishDate, false);
+  
+  function finishDate(){
+    if(checkDate.checked){
+        on();
+    }else{
+       off();
     }
-}
-
-// Injectar 
-
-const labelEnd = document.createElement('label');
-const salto = document.createElement('br');
-const inputDateEnd = document.createElement('input');
-
-inputDateEnd.setAttribute("type","datetime-local");
-inputDateEnd.setAttribute("id","endData_input");
-labelEnd.textContent = 'End date:';
-
-endDate.appendChild(salto);
-endDate.appendChild(labelEnd);
-endDate.appendChild(inputDateEnd);
+  }
+// APPEAR OR DISAPPEAR REMIND EVENT
+function appear(){
+    reminder.style.display = "block";
+  }
+  
+  function disappear(){
+    reminder.style.display = "none";
+  }
+  
+  checkReminder.addEventListener("change", appearRemind, false);
+  
+  function appearRemind(){
+    if(checkReminder.checked){
+        appear();
+    }else{
+       disappear();
+    }
+  }
