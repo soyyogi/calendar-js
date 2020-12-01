7f52028d843b1f7835768c90015760028d51bbfa
wsierra010 <wsierra010@gmail.com>
2020-12-01 18:10:24 +0100

    Modal PopUp CSS and JavaScript #3

--- functionValidation.js
+++ functionValidation.js
@@ -1,16 +1,40 @@
 
 const formulario = document.getElementById("formulario");
+// BUTTON FOR CREATE NEW EVENT
+const createEvent = document.getElementById("create_event");
+// APPEAR EVENT FORM
+const formEvent = document.getElementById("event_form");
+// TITLE EVENT INPUT
+const nameEvent = document.getElementById("title_input");
+// DISAPPEAR EVENT FORM
+const closeEvent = document.getElementById("cross");
 // END DATE VARIABLES 
 const endDate = document.getElementById("newDateEnd");
 const checkDate = document.getElementById("checked_endDate");
 // REMIND EVENT VARIABLES
 const reminder = document.getElementById("remindChoose");
 const checkReminder = document.getElementById("check_remind");
+// MODAL CONTAINER 
+const modal_container = document.getElementById("modal_container");
 
 
 formulario.addEventListener("submit", (e) => {
     e.preventDefault();
 })
+
+// FORM VALIDATION
+
+
+// Create NEW EVENT FORM
+createEvent.addEventListener('click', () => {
+    modal_container.classList.add('show');
+    formEvent.style.display="block"; 
+  });
+
+closeEvent.onclick = function(){
+    formEvent.style.display="none";
+}
+    
 
 
 // APPEAR OR DISAPPEAR DATE END CHOOSE
