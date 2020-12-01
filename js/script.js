4e9bec07b96ccacf0a35027e9ae4cfc286ae8f56
yogi <raoyogesh78@yahoo.in>
2020-12-01 09:00:49 +0100

    create next month days using js #6

--- js/script.js
+++ js/script.js
@@ -4,13 +4,6 @@ const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
 
 function renderCalendar() {
     document.querySelector('.current_month').innerHTML = months[currentDate.getMonth()];
-
-    const lastDate = (new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0)).getDate();
-
-    currentDate.setDate(1);
-    const firstDay = currentDate.getDay(); //index start at 0 which is sunday
-
-    const lastDatePrevMonth = (new Date(currentDate.getFullYear(), currentDate.getMonth(), 0)).getDate();
     
     //rendering previous_month_days
     for(let i = firstDay; i > 0; i--){
@@ -22,9 +15,21 @@ function renderCalendar() {
         document.querySelector('.days').innerHTML += `<div class="current_month_day">${i}</div>`
     }
 
+    //rendering next_month_days
+    for(let i = 1; i < lastday; i++){
+        document.querySelector('.days').innerHTML += `<div class="next_month_day">${i}</div>`
+    }
 }
 
+const lastDate = (new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0)).getDate();
 
+currentDate.setDate(1);
+const firstDay = currentDate.getDay(); //index start at 0 which is sunday
+
+const lastDatePrevMonth = (new Date(currentDate.getFullYear(), currentDate.getMonth(), 0)).getDate();
+
+currentDate.setDate(lastDate)
+const lastday = currentDate.getDay()
 
 
 renderCalendar()
