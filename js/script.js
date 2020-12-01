acb875bf39953903888e6f9c5353bb11bc66f949
yogi <raoyogesh78@yahoo.in>
2020-12-01 08:32:25 +0100

    create current month days using js #6

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
-const currentDate = new Date();
+let currentDate = new Date();
 
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-document.querySelector('.current_month').innerHTML = months[currentDate.getMonth()];
+function renderCalendar() {
+    document.querySelector('.current_month').innerHTML = months[currentDate.getMonth()];
 
+    for(let i = 1; i <= lastDay; i++){
+        document.querySelector('.days').innerHTML += `<div class="current_month_day">${i}</div>`
+    }
+}
+
+const lastDay = (new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0)).getDate();
+
+
+renderCalendar()
